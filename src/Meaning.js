d847bc6e147503b1582b86ccaaaafb5650ec0237
Katie <kaytay418@live.com>
2022-07-28 13:37:57 -0700

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,22 +1,25 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
+
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
-              {definition.definition}
+              <strong>Definition:</strong> {definition.definition}
               <br />
-              <em>{definition.example}</em>
+              <strong>Example:</strong> <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
       })}
-      <p>{props.meaning.definitions[0].definition}</p>
-      <p>{props.meaning.definitions[0].example}</p>
     </div>
   );
 }
---
+++ src/Synonyms.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms.length !== 0) {
+    return (
+      <div>
+        <strong>Synonyms:</strong>
+        <ul className="synonyms">
+          {props.synonyms.map(function (synonyms, index) {
+            return <li key={index}>{synonyms}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
